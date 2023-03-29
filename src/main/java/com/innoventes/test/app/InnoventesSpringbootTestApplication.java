04c7ec4333b71ace0effb8e1e52df26fd03d2c80
Umasuthan Ramakrishnan <umasuthan@innoventes.co>
2023-03-29 12:35:02 +0530

    Initial Commit

---
+++ src/main/java/com/innoventes/test/app/InnoventesSpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package com.innoventes.test.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoventesSpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoventesSpringbootTestApplication.class, args);
+	}
+
+}
